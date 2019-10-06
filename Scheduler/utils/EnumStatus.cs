067404896b6b94eed74eccf0493a433fef2988d8
Davoleo <davoleo2001@gmail.com>
2019-10-06 16:31:21 +0200

    Internal Data Structure

---
+++ Scheduler/utils/EnumStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.utils
+{
+    public enum EnumStatus
+    {
+        ACTIVE,
+        IDLE,
+        BLOCKED,
+        COMPLETE,
+        INACTIVE
+    }
+}
---
+++ Scheduler/utils/Task.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduler.utils
+{
+    class Task
+    {
+        public int ID { get; set; }
+        public string ProjectName { get; set; }
+        public EnumStatus Status { get; set; }
+        public string Type { get; set; }
+        public bool TODO { get; set; }
+
+        public Task(int id, string name, EnumStatus status, string type, bool todo)
+        {
+            ID = id;
+            ProjectName = name;
+            Status = status;
+            Type = type;
+            TODO = todo;
+        }
+
+    }
+}
